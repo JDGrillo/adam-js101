1608b4967711d4c1e76f370876cb0ec3fff7e431
JD Grillo <jdgrillo@JDs-MacBook-Air.local>
2019-09-14 12:28:24 -0500

    first commit

--- exercises/level1.js
+++ exercises/level1.js
@@ -1,34 +1,45 @@
 function lvl1exercise1 () {
   // Declare a variable without instantiating it and return it!
-
+  let a
+  return a
 }
 
 function lvl1exercise2 () {
   // Declare and instantiate a number and return it
-
+  let num = 3
+  return num
 }
 
 function lvl1exercise3 () {
   // Declare and instantiate a floating point number that is not a whole number and return it
-
+  let float = 4.1
+  return float
 }
 
 function lvl1exercise4 () {
   // Declare and instantiate a string "Hello World!" and return it
-
+  let hw = "Hello World!"
+  return hw
 }
 
 function lvl1exercise5 () {
   // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
+  let arr = ["Hello World!", 4]
+  return arr
 
 }
 
 function lvl1exercise6 () {
   // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
-
+  let obj = {
+    key1 : "Hello World!",
+    key2 : 4
+  }
+  return obj
 }
 
 function lvl1exercise7 () {
   // Declare and instantiate a boolean value 'false' and return it
-
+  let bool = false
+  return bool
 }
